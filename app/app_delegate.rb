24863474de0ac7d09be3985752e6ffce7428f6ca
Daniel Morris <dan@sofarsounds.com>
2015-11-14 15:20:30 +0000

    working with window factory

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,18 +1,41 @@
 class AppDelegate
-  def application(application, didFinishLaunchingWithOptions:launchOptions)
-    rootViewController = UIViewController.alloc.init
-    rootViewController.title = 'Hello'
-    rootViewController.view.backgroundColor = UIColor.whiteColor
+  def hello_world_label
+    @hello_world_label ||= begin
+      frame = CGRectMake(20,200,280,40)
+      label           = UILabel.alloc.initWithFrame(frame)
+      label.text      = "Hello world"
+      label.textColor = UIColor.whiteColor
+      label.textAlignment = UITextAlignmentCenter
+      label
+    end
+  end
 
-    navigationController = UINavigationController.alloc.initWithRootViewController(rootViewController)
+  def window
+    unless @window
+      # setup root controller
+      rootViewController = UIViewController.alloc.init
+      rootViewController.title = 'Hello'
+      rootViewController.view.backgroundColor = UIColor.blackColor
+      # setup nav controller
+      navigationController = UINavigationController.alloc.initWithRootViewController(rootViewController)
+      # setup window
+      @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+      # assign root controller
+      @window.rootViewController = navigationController
+    end
 
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = navigationController
-    @window.makeKeyAndVisible
+    @window
+  end
 
+  def show_alert
     alert = UIAlertView.new
     alert.message = "Hello World!"
     alert.show
+  end
+
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    # window.addSubview(hello_world_label)
+    window.makeKeyAndVisible
 
     true
   end
